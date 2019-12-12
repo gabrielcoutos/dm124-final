34d503909a38310147352c8b8f473d471d90dc7f
Gabriel <gabrielcoutos@yahoo.com.br>
2019-12-12 10:32:26 -0300

    Add not-found and middlewares

--- src/app.js
+++ src/app.js
@@ -2,9 +2,18 @@
 const express = require('express');
 const app = express();
 
-
+// Middlewares
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const notFound = require('./middleware/not-found');
 // Routes
 const taskRouter = require('./routes/tasks');
+
+app.use(cors());
+app.use(helmet());
+app.use(morgan('dev'));
 app.use('/tasks',taskRouter)
+app.use(notFound);
 
 module.exports = app;
